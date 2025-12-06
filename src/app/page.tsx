60b01e7d1b85839cb01c6d95da971b465bb7e20e
齋藤将平 <shohei@shohei-MacBook.local>
2025-12-06 18:40:05 +0900

    20251206-4 タスク入力画面追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="fixed top-16 left-70">
--- src/app/tsk_input/page.tsx
+++ src/app/tsk_input/page.tsx
@@ -1,7 +1,27 @@
 export default function Tsk_Input(){
     return(
-        <div className="fixed top-16 left-70">
-            <h1>タスクを入力してください</h1>
+    <div className="fixed top-25 left-64 right-64 h-[350px] flex justify-center">
+        <div className="border border-gray-300 rounded p-4 w-150">
+            <h1 className="mb-2">タスク</h1>
+                <input
+                    type="text"
+                    placeholder="タスク"
+                    className="border border-gray-300 rounded p-2 w-100 mb-6"
+                />
+
+            <h1 className="mb-2">期日</h1>
+                <input 
+                    type="date"
+                    placeholder="XXXX-YY-ZZ"
+                    className="border border-gray-300 rounded p-2 w-100 mb-6"
+                />
+
+            <button
+                className="block border border-gray-300 p-3 rounded hover:bg-gray-200"
+            >
+                追加
+            </button>
         </div>
+    </div>
     );
 }
