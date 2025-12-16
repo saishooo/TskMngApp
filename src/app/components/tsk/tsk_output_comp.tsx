de4fe383ef30c334d3d6d24f7b4597e101be7a09
齋藤将平 <shohei@shohei-MacBook.local>
2025-12-16 22:07:23 +0900

    20251216-4

--- src/app/components/tsk/tsk_output_comp.tsx
+++ src/app/components/tsk/tsk_output_comp.tsx
@@ -12,6 +12,7 @@ export default function Tsk_Output_Completed() {
   const [ editTitle,  setEditTitle ] = useState( "" );
   const [ editDeadLine, setEditDeadLine ] = useState( "" );
 
+  //ラジオボタン
   const TaskRadioButton = ( { task }: { task: Tasks } ) => (
     <div className="flex w-10 h-15 px-4">
         <input
@@ -38,10 +39,9 @@ export default function Tsk_Output_Completed() {
                 <p>There are no completed tasks.</p>
                 ) : (
                 completed_tsk.map(( task ) => (
-                  <div key={ task.id } className="flex">
+                    <div key={ task.id } className="flex">
                       { !isEnditing && (
                         <div className="flex">
-
                           <TaskRadioButton task={ task } />
                           <div className="flex items-center justify-between w-110 px-4 my-2 hover:bg-gray-200 rounded" 
                             onClick = {() => setIsEditing( true )}
@@ -73,43 +73,43 @@ export default function Tsk_Output_Completed() {
                         <div className="flex">
                             <TaskRadioButton task={ task } />
     
-                          <div className="flex items-center justify-between w-110 px-4 my-2 hover:bg-gray-200 rounded"
-                          onClick = {() => setIsEditing( false )}>
+                            <div className="flex items-center justify-between w-110 px-4 my-2 hover:bg-gray-200 rounded"
+                                onClick = {() => setIsEditing( false )}>
                             
-                            <div>
-                              <input
-                              className="block font-semibold h-10"
-                              value={ editTitle }
-                              placeholder={ task.tsk_title }
-                              onChange = { ( e ) => setEditTitle( e.target.value ) }
-                              onClick  = { ( e ) => e.stopPropagation()}
-                              />
-                              <input
-                              className="block text-sm text-gray-700 h-10"
-                              type="date"
-                              value={editDeadLine}
-                              placeholder={ task.dead_line }
-                              onChange = { ( e ) => setEditDeadLine( e.target.value ) }
-                              onClick  = { ( e ) => e.stopPropagation()}
-                              />
+                                <div>
+                                    <input
+                                    className="block font-semibold h-10"
+                                    value={ editTitle }
+                                    placeholder={ task.tsk_title }
+                                    onChange = { ( e ) => setEditTitle( e.target.value ) }
+                                    onClick  = { ( e ) => e.stopPropagation()}
+                                    />
+                                    <input
+                                    className="block text-sm text-gray-700 h-10"
+                                    type="date"
+                                    value={editDeadLine}
+                                    placeholder={ task.dead_line }
+                                    onChange = { ( e ) => setEditDeadLine( e.target.value ) }
+                                    onClick  = { ( e ) => e.stopPropagation()}
+                                    />
+                                </div>
                             </div>
-                          </div>
     
-                          <div className="flex flex-col items-center justify-center w-20 rounded hover:bg-green-200">
-                            <button
-                              onClick={ () => Local_UpdateTask( task.id, editTitle, editDeadLine ) }
-                              >
-                                <Image 
-                                  className="mt-1" 
-                                  src="/update.svg" 
-                                  alt="delete" 
-                                  width={24} 
-                                  height={24} />
-                              </button>
-                              <p className="text-xs mt-1">Update</p>
+                            <div className="flex flex-col items-center justify-center w-20 rounded hover:bg-green-200">
+                                <button
+                                onClick={ () => Local_UpdateTask( task.id, editTitle, editDeadLine ) }
+                                >
+                                    <Image 
+                                    className="mt-1" 
+                                    src="/update.svg" 
+                                    alt="delete" 
+                                    width={24} 
+                                    height={24} />
+                                </button>
+                                <p className="text-xs mt-1">Update</p>
                             </div>
-                          </div>            
-                      )}
+                        </div>            
+                        )}
                     </div>
                 ))
               )}
