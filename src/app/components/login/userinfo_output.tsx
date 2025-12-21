b64a218e9a218b79fe2fa3cd8c663da19617f1db
齋藤将平 <shohei@shohei-MacBook.local>
2025-12-21 21:21:06 +0900

    20251221-13

--- src/app/components/login/userinfo_output.tsx
+++ src/app/components/login/userinfo_output.tsx
@@ -7,11 +7,18 @@ export function UserInfo_Output() {
     const { user } = useAuth();
     return(
         <div className={ headerInnerClass }>
-            <div className="w-100 h-15 mt-3 ml-3  border rounded border-gray-300">
+            <div className="flex items-center w-90 h-13 mt-3 ml-3  border rounded border-gray-300">
                 { user? (
-                    <h1>{ user.user_name }</h1>
+                    <div className="flex ml-3">
+                        <h1 className="font-bold">Login Now :</h1>
+                        <p className="ml-2">{ user.user_name }</p>
+                        <p className="ml-2">さん</p>
+                    </div>
                 ) : (
-                    <h1>Logout Now</h1>
+                    <div className="flex ml-3">
+                        <h1 className="font-bold">Logout Now</h1>
+                        <p className="ml-2">Guest</p>
+                    </div>
                 )}
             </div>
         </div>
