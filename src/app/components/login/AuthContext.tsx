0f0b0390f9d2dae7d2bdbc7f200c110035b3e71a
齋藤将平 <shohei@shohei-MacBook.local>
2025-12-22 22:20:45 +0900

    20251222-3

--- src/app/components/login/AuthContext.tsx
+++ src/app/components/login/AuthContext.tsx
@@ -36,7 +36,7 @@ export const AuthProvider = ( { children }: { children: ReactNode }) => {
 
     //ログアウト関数
     const logout = () => {
-        setUser( null );
+        setUser( guestUser );
         localStorage.removeItem( "currentUser" );
     }
 
