15c1cbeb02f8a28eb0ea8000c62a548e3c25c192
齋藤将平 <shohei@shohei-MacBook.local>
2025-12-24 22:13:47 +0900

    20251224-8

--- src/app/components/log/loginfo_output.tsx
+++ src/app/components/log/loginfo_output.tsx
@@ -9,34 +9,34 @@ export function LogInfo_Output() {
   const isLoggedIn = user.user_id !== "guest";
   const router = useRouter();
 
-  const Login_out_button =
+  const Login_button =
     "ml-8 w-16 h-10 text-white font-bold rounded bg-gray-600 hover:bg-gray-400";
-
+  const Logout_button =
+    "ml-14 w-16 h-10 text-white font-bold rounded bg-gray-600 hover:bg-gray-400";
   const SignUp_button =
     "ml-4 w-20 h-10 text-white font-bold rounded bg-gray-600 hover:bg-gray-400";
 
   return (
     <div className={headerInnerClass}>
-      <div className="flex items-center w-115 h-13 mt-3 ml-3  border rounded border-gray-300">
         {isLoggedIn ? (
-          <>
+          <div className="flex items-center w-90 h-13 mt-3 ml-3  border rounded border-gray-300">
             <div className="flex ml-3 w-55">
               <h1 className="font-bold text-green-600">Login Now :</h1>
               <p className="ml-2">{user.user_name}</p>
               <p className="ml-2">さん</p>
             </div>
-            <button className={Login_out_button} onClick={logout}>
+            <button className={Logout_button} onClick={logout}>
               Logout
             </button>
-          </>
+          </div>
         ) : (
-          <>
+          <div className="flex items-center w-115 h-13 mt-3 ml-3  border rounded border-gray-300">
             <div className="flex ml-3 w-55">
               <h1 className="font-bold text-blue-600">Logout Now :</h1>
               <p className="ml-2">Guest</p>
             </div>
             <button
-              className={Login_out_button}
+              className={Login_button}
               onClick={() => router.push("/log/login")}
             >
               Login?
@@ -48,9 +48,8 @@ export function LogInfo_Output() {
             >
               Sing Up?
             </button>
-          </>
+          </div>
         )}
-      </div>
     </div>
   );
 }
