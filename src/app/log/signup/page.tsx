1363b304c18b244b96987dc570fbc5fb63aebe69
齋藤将平 <shohei@shohei-MacBook.local>
2025-12-24 20:14:16 +0900

    20251224-2

--- src/app/log/signup/page.tsx
+++ src/app/log/signup/page.tsx
@@ -1,5 +1,4 @@
 import TskMngApp_SignUp from "@/app/components/log/signup";
-import { LogInfo_Output } from "@/app/components/log/loginfo_output";
 
 export default function Tsk_Login_Page(){
   return(
