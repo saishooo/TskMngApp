4f4ae62c5b1dadb85bdadceec1fafdfc37351bdc
齋藤将平 <shohei@shohei-MacBook.local>
2025-12-25 22:38:47 +0900

    20251225-6

--- src/app/components/tsk/tsk_output_all.tsx
+++ src/app/components/tsk/tsk_output_all.tsx
@@ -1,27 +1,18 @@
 "use client";
 
 import { useTasks } from "./tsk_parent";
-import { useState } from "react";
-import { Tasks } from "@/app/types";
 import Image from "next/image";
 import {
   headerInnerClass_center,
-  taskBox,
   taskOutput_taskDisplayArea,
   taskOutput_deleteButton,
-  taskOutput_updateButton,
   taskBoxBig,
 } from "@/app/className";
 import { useAuth } from "../log/AuthContext";
 
 export default function Tsk_Output_AllList() {
-  const { tasks, updateTask, deleteTask, toggleTask } = useTasks();
+  const { tasks, deleteTask, } = useTasks();
   const { user } = useAuth();
-
-  const [editingTaskId, setEditingTaskId] = useState<number | null>(null);
-  const [editTitle, setEditTitle] = useState("");
-  const [editDeadLine, setEditDeadLine] = useState("");
-  const [editPriority, setEditPriority] = useState("");
 
   const completedIcon = (
     <Image
@@ -32,19 +23,6 @@ export default function Tsk_Output_AllList() {
       height={24}
     />
   );
-
-  //アップデート関数
-  const Local_UpdateTask = (
-    id: number,
-    newTitle: string,
-    newDeadLine: string,
-    newPriority: string
-  ) => {
-    updateTask(id, newTitle, newDeadLine, newPriority);
-    setEditingTaskId(null);
-    setEditTitle("");
-    setEditDeadLine("");
-  };
 
   //ログインしているユーザーID別にタスクを吸い上げ
   const all_user_tsk = tasks.filter((task) => task.user_id === user.user_id);
@@ -60,7 +38,6 @@ export default function Tsk_Output_AllList() {
           ) : (
             all_user_tsk.map((task) => (
               <div key={task.id} className="flex">
-                {editingTaskId !== task.id && (
                   <div className="flex">
                     <div className="flex items-center justify-center w-10 h-15">
                       <p>{task.comp ? completedIcon : ""}</p>
@@ -88,7 +65,6 @@ export default function Tsk_Output_AllList() {
                       <p className="text-xs mt-1">delete</p>
                     </div>
                   </div>
-                )}
               </div>
             ))
           )}
