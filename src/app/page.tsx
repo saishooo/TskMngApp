a3695e268596e13444f75f2acfafd2b577ad7859
齋藤将平 <shohei@shohei-MacBook.local>
2025-12-27 18:30:29 +0900

    20251227-8

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,13 @@ import { headerInnerClass } from "./className";
 export default function Home() {
   return (
     <div className={headerInnerClass}>
-      <h1>Development of TskManagerApp started on 2025-12-06 at 00:06.</h1>
+      <div className="block">
+        <p className="mb-3">Development of TskManagerApp started on 2025-12-06 at 00:06.</p>
+        <p>We have created a task management tool.</p>
+        <p>Please log in or sign up first, then start adding your tasks.</p>
+        <p>You can also add tasks as a guest.</p>
+        <p>You can organize your daily tasks and easily track your progress.</p>
+      </div>
     </div>
   );
 }
