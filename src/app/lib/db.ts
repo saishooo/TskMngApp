f71cabe17a76e0753f78a6526413caab645f55cd
齋藤将平 <shohei@shohei-MacBook.local>
2026-01-17 00:26:34 +0900

    20260117 データベース管理するための関数を定義

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -1,2 +1,20 @@
 import Database from "better-sqlite3";
-export const db = new Database("todos.db");
+import path from "path";
+
+const dbPath = path.join(process.cwd(), "data", "todos.db");
+
+export const db = new Database(dbPath);
+
+//起動時にテーブル作成をする
+db.exec(`
+    CREATE TABLE IF NOT EXISTS tasks (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    comp INTRGER DEFAULT 0,
+    tsk_title TEXT NOT NULL,
+    dead_line TEXT,
+    priority TEXT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    user_id TEXT NOT NULL
+    );
+    `);
