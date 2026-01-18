42140a3042650da575928c352fe84074513e3953
齋藤将平 <shohei@shohei-MacBook.local>
2026-01-18 22:51:39 +0900

    20260118 日本語に変更しました(微修正)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 //最初に表示される紹介ージ
 
 const descriptions = [
-  "タスク管理アプリの開発は 2025年12月6日 00:06 に開始されました",
+  "タスク管理アプリの開発は 2025年12月6日 00:06 に開始されました。",
   "私はタスク管理ツールを作成しました。",
   "まずはログイン、またはサインアップを行ってからタスクの追加を開始してください。",
   "ゲストとしてタスクを追加することも可能です。",
-  "日々のタスクを整理し、進捗を簡単に把握できます。",
+  "日々のタスクを整理し、進捗を把握できます。",
 ];
 
 const headerInner_className = "fixed top-16 flex justify-center";
-const documentBase_output = "items-center pt-20 pl-10";
+const documentBase_output = "items-center pt-20 pl-2 pr-2";
 const documentLg_output = "lg:pt-16 lg:pl-74";
 
 export default function Introduction() {
